bbe6c92c5a2c06373d08b872a9d5f01b465aa889
Euncho Bae <yuen6482@gmail.com>
2025-08-21 16:59:53 +0900

    Add PlayerFactory

--- mission2/mission2/attendance.cpp
+++ mission2/mission2/attendance.cpp
@@ -14,38 +14,96 @@ using namespace std;
 
 map<string, Player*> playerMap;
 
-string getGradeInString(int grade)
-{
-	const string gradeString[3] = { "NORMAL" , "GOLD", "SILVER" };
-	return gradeString[grade];
-}
-
-eDay convertDayStringToInt(string day)
-{
-	if (day == "monday") {
-		return eMonday;
-	}
-	if (day == "tuesday") {
-		return eTuesday;
-	}
-	if (day == "wednesday") {
-		return eWednesday;
-	}
-	if (day == "thursday") {
-		return eThursday;
-	}
-	if (day == "friday") {
-		return eFriday;
-	}
-	if (day == "saturday") {
-		return eSaturday;
-	}
-	if (day == "sunday") {
-		return eSunday;
+class StringConverter {
+private:
+	StringConverter() {}
+public:
+	static StringConverter* getInstance() {
+		static StringConverter stringConverter;
+		return &stringConverter;
 	}
 
-	return eDayError;
-}
+	string getGradeInString(int grade)
+	{
+		const string gradeString[3] = { "NORMAL" , "GOLD", "SILVER" };
+		return gradeString[grade];
+	}
+
+	eDay convertDayStringToInt(string day)
+	{
+		if (day == "monday") {
+			return eMonday;
+		}
+		if (day == "tuesday") {
+			return eTuesday;
+		}
+		if (day == "wednesday") {
+			return eWednesday;
+		}
+		if (day == "thursday") {
+			return eThursday;
+		}
+		if (day == "friday") {
+			return eFriday;
+		}
+		if (day == "saturday") {
+			return eSaturday;
+		}
+		if (day == "sunday") {
+			return eSunday;
+		}
+
+		return eDayError;
+	}
+};
+
+class PlayerFactory
+{
+public:
+	map<string, Player*> playerMap;
+
+	Player* getPlayer(string name)
+	{
+		if (playerMap.count(name) == 0) {
+			//insert in map
+			playerMap.insert({ name, new Player(name, playerMap.size() + 1) });
+		}
+		return playerMap[name];
+	}
+
+	void updateAttendance(string name, string dayInString) {
+		int dayInInt = StringConverter::getInstance()->convertDayStringToInt(dayInString);
+		Player* player = getPlayer(name);
+		player->updateAttendance(dayInInt);
+	}
+
+	void printPointAndGrade() {
+		for (auto iter = playerMap.begin(); iter != playerMap.end(); iter++)
+		{
+			Player* player = iter->second;
+			cout << "NAME : " << player->getName() << ", ";
+			cout << "POINT : " << player->calculateFinalPoint() << ", ";
+			cout << "GRADE : " << StringConverter::getInstance()->getGradeInString(player->getGrade()) << "\n";
+		}
+		std::cout << "\n";
+	}
+
+	void printRemovedPlayers() {
+		std::cout << "Removed player\n";
+		std::cout << "==============\n";
+		for (auto iter = playerMap.begin(); iter != playerMap.end(); iter++)
+		{
+			Player* player = iter->second;
+			if (player->checkGradeNormal() && player->checkAttendanceNotEnough()) {
+				std::cout << player->getName() << "\n";
+			}
+		}
+	}
+
+	int getPlayerMapSize() {
+		return playerMap.size();
+	}
+};
 
 Player* getPlayer(string name)
 {
@@ -57,7 +115,7 @@ Player* getPlayer(string name)
 }
 
 void updateAttendance(string name, string dayInString) {
-	int dayInInt = convertDayStringToInt(dayInString);
+	int dayInInt = StringConverter::getInstance()->convertDayStringToInt(dayInString);
 	Player* player = getPlayer(name);
 	player->updateAttendance(dayInInt);
 }
@@ -68,7 +126,7 @@ void printPointAndGrade() {
 		Player* player = iter->second;
 		cout << "NAME : " << player->getName() << ", ";
 		cout << "POINT : " << player->calculateFinalPoint() << ", ";
-		cout << "GRADE : " << getGradeInString(player->getGrade()) << "\n";
+		cout << "GRADE : " << StringConverter::getInstance()->getGradeInString(player->getGrade()) << "\n";
 	}
 	std::cout << "\n";
 }
@@ -103,8 +161,13 @@ void ResetAllData(){
 class TestFixture : public ::testing::Test
 {
 public:
+	PlayerFactory* factory;
 	void SetUp() override {
-		ResetAllData();
+		factory = new PlayerFactory();
+	}
+
+	void TearDown() override {
+		delete(factory);
 	}
 
 	void getInputAndUpdateAttendance() {
@@ -113,7 +176,7 @@ public:
 		for (int i = 0; i < 500; i++) {
 			string name, attendDay;
 			fin >> name >> attendDay;
-			updateAttendance(name, attendDay);
+			factory->updateAttendance(name, attendDay);
 		}
 	}
 };
@@ -141,56 +204,57 @@ TEST_F(TestFixture, CheckRemovedPlayersRight)
 
 TEST_F(TestFixture, CheckGetIdProcess)
 {
-	Player* player = getPlayer("Ian");
-	Player* player2 = getPlayer("Ian"); // expect same id
-	Player* player3 = getPlayer("Susan");
+	Player* player = factory->getPlayer("Ian");
+	Player* player2 = factory->getPlayer("Ian"); // expect same id
+	Player* player3 = factory->getPlayer("Susan");
 
 	EXPECT_EQ(1, player->getId());
 	EXPECT_EQ(1, player2->getId());
 	EXPECT_EQ(2, player3->getId());
-	EXPECT_EQ(2, playerMap.size());
+	EXPECT_EQ(2, factory->getPlayerMapSize());
 }
 
 TEST_F(TestFixture, CheckUpdateAttendance)
 {
-	updateAttendance("Harry", "wednesday");
-	EXPECT_EQ(1, playerMap.size());
-	EXPECT_EQ("Harry", playerMap["Harry"]->getName());
-	EXPECT_EQ(1, playerMap["Harry"]->getId());
-	EXPECT_EQ(1, playerMap["Harry"]->getAttendance(eWednesday));
+	factory->updateAttendance("Harry", "wednesday");
+
+	EXPECT_EQ(1, factory->getPlayerMapSize());
+	EXPECT_EQ("Harry", factory->getPlayer("Harry")->getName());
+	EXPECT_EQ(1, factory->getPlayer("Harry")->getId());
+	EXPECT_EQ(1, factory->getPlayer("Harry")->getAttendance(eWednesday));
 }
 
 TEST_F(TestFixture, CheckAttendanceInputWellManaged)
 {
 	getInputAndUpdateAttendance();
 
-	EXPECT_EQ(19, playerMap.size());
+	EXPECT_EQ(19, factory->getPlayerMapSize());
 }
 
 TEST_F(TestFixture, CheckPointWellCalculated)
 {
 	getInputAndUpdateAttendance();
 
-	EXPECT_EQ(19, playerMap.size());
-	EXPECT_EQ(127, playerMap["Hannah"]->calculateFinalPoint());
+	EXPECT_EQ(19, factory->getPlayerMapSize());
+	EXPECT_EQ(127, factory->playerMap["Hannah"]->calculateFinalPoint());
 }
 
 TEST_F(TestFixture, CalculateOnceTest)
 {
 	getInputAndUpdateAttendance();
 
-	EXPECT_EQ(19, playerMap.size());
-	EXPECT_EQ(127, playerMap["Hannah"]->calculateFinalPoint());
+	EXPECT_EQ(19, factory->getPlayerMapSize());
+	EXPECT_EQ(127, factory->playerMap["Hannah"]->calculateFinalPoint());
 }
 
 TEST_F(TestFixture, CheckGradeWellCalculated)
 {
 	getInputAndUpdateAttendance();
 
-	EXPECT_EQ(19, playerMap.size());
-	EXPECT_EQ(eGold, playerMap["Hannah"]->getGrade());
-	EXPECT_EQ(eSilver, playerMap["George"]->getGrade());
-	EXPECT_EQ(eNormal, playerMap["Quinn"]->getGrade());
+	EXPECT_EQ(19, factory->getPlayerMapSize());
+	EXPECT_EQ(eGold, factory->playerMap["Hannah"]->getGrade());
+	EXPECT_EQ(eSilver, factory->playerMap["George"]->getGrade());
+	EXPECT_EQ(eNormal, factory->playerMap["Quinn"]->getGrade());
 }
 
 TEST_F(TestFixture, CheckGradeNormal)
